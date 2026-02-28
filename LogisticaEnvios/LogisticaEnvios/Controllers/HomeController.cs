d25fdc4a26dc356ad1bce6571993359a2fb629da
castaxyz <castaxyz@gmail.com>
2026-02-27 20:50:37 -0800

    mensaje en HomeController desde rama A

--- LogisticaEnvios/LogisticaEnvios/Controllers/HomeController.cs
+++ LogisticaEnvios/LogisticaEnvios/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace LogisticaEnvios.Controllers
         [HttpGet(Name = "GetHome")]
         public string Get()
         {
-            return "Tercer ERROR intencional desde Visual Studio";
+            return "Mensaje desde rama A";
         }
     }
 }
