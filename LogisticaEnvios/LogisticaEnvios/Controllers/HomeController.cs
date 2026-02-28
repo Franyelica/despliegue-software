dd6e77dbdffb1f2503e5ecfb48fa28bc50c58553
castaxyz <castaxyz@gmail.com>
2026-02-27 20:22:50 -0800

    Revert "Update HomeController.cs"
    This reverts commit 54a6e778215f4a1fe4ee9cdf9c9147d1c51c2ba6.

--- LogisticaEnvios/LogisticaEnvios/Controllers/HomeController.cs
+++ LogisticaEnvios/LogisticaEnvios/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace LogisticaEnvios.Controllers
         [HttpGet(Name = "GetHome")]
         public string Get()
         {
-            return "ERROR intencional desde Git Desktop";
+            return "Nuevo mensaje desde la correción";
         }
     }
 }
